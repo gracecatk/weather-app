e0da6a1fb6b7de9a5cbc358eed0dc63bb16a2b0b
gracecatk <gracie.kiker@gmail.com>
2023-10-01 19:24:04 -0500

    added 5 day forecast

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const apiKey = "8c4febd32c6b1c91aee4fdb805d2b5ea";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q="; // Use "imperial" units for Fahrenheit
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".Weather-icon");
+const weatherIcon = document.querySelector(".weather-icon");
+const forecastList = document.querySelector(".forecast-list");
 
 // Function to save the city name to local storage
 function saveCityToLocalStorage(city) {
@@ -15,34 +16,56 @@ function getCityFromLocalStorage() {
   return localStorage.getItem("cityName");
 }
 
-// Function to update the UI with weather data
+// Function to update the UI with weather data for the next 5 days
 async function updateWeatherUI(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
   if (response.status === 404) {
     document.querySelector(".error").style.display = "block";
-    document.querySelector(".Weather").style.display = "none";
+    document.querySelector(".weather").style.display = "none";
+    forecastList.innerHTML = ""; // Clear the forecast list
   } else {
     let data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    // Update the current weather data
+    document.querySelector(".city").innerHTML = data.city.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.list[0].main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.list[0].main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.list[0].wind.speed + " mph";
 
-    if (data.weather[0].main === "Clouds") {
+    if (data.list[0].weather[0].main === "Clouds") {
       weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main === "Clear") {
+    } else if (data.list[0].weather[0].main === "Clear") {
       weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main === "Rain") {
+    } else if (data.list[0].weather[0].main === "Rain") {
       weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main === "Drizzle") {
+    } else if (data.list[0].weather[0].main === "Drizzle") {
       weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main === "Mist") {
+    } else if (data.list[0].weather[0].main === "Mist") {
       weatherIcon.src = "images/mist.png";
     }
 
-    document.querySelector(".Weather").style.display = "block";
+    // Update the forecast for the next 5 days
+    forecastList.innerHTML = ""; // Clear the forecast list
+
+    for (let i = 0; i < 5; i++) {
+      const forecast = data.list[i];
+      const date = new Date(forecast.dt * 1000);
+      const day = date.toLocaleDateString("en-US", { weekday: "short" });
+      const temp = Math.round(forecast.main.temp) + "°F"; 
+      const icon = forecast.weather[0].main.toLowerCase();
+
+      const forecastItem = document.createElement("div");
+      forecastItem.classList.add("forecast-item");
+      forecastItem.innerHTML = `
+        <div class="day">${day}</div>
+        <img src="images/${icon}.png" alt="${forecast.weather[0].description}">
+        <div class="temp">${temp}</div>
+      `;
+      forecastList.appendChild(forecastItem);
+    }
+
+    document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
   }
 }
